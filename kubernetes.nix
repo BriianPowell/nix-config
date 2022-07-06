16869822f65c8605e1589c3dea7e5d09709f5af9
Louis Orleans <louis@orleans.io>
2022-07-05 22:55:10 -0700

    ✨ run containerd

--- kubernetes.nix
+++ kubernetes.nix
@@ -15,8 +15,25 @@
     autoPrune.enable = true;
   };
 
+  virtualisation.containerd = {
+    enable = true;
+    # settings = {
+    #   version = 2;
+    #   plugins."io.containerd.grp.v1.cri" = {
+    #     cni.conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+    #   };
+    # };
+  };
+
   services.k3s = {
     enable = true;
     role = "server";
+    # docker = true;
+    extraFlags = "--container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  };
+
+  systemd.services.k3s = {
+    wants = [ "containerd.service" ];
+    after = [ "containerd.service" ];
   };
 }
