e6eecb12f6cd2ff4634c46d678af60a3c199e991
Brian Powell <brian@powell.place>
2023-02-05 18:56:33 -0800

    Adding Sanoid for ZFS snapshots

--- fs.nix
+++ fs.nix
@@ -15,5 +15,23 @@
 
   services.zfs.trim.enable = true;
   services.zfs.autoScrub.enable = true;
-  # TODO: services.sanoid
+
+  services.sanoid = {
+    enable = true;
+    templates.production = {
+      frequently = 0;
+      hourly = 36;
+      daily = 30;
+      monthly = 3;
+      yearly = 0;
+      autoprune = true;
+      autosnap = true;
+    };
+
+    datasets.moriyya = {
+      useTemplate = [ "production" ];
+      recursive = true;
+      processChildrenOnly = true;
+    };
+  };
 }
