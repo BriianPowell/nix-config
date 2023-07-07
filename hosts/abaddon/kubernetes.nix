74321247a13ff3af816b4ee4ac855c1f3d9598c3
Brian Powell <brian@powell.place>
2023-07-07 15:23:52 -0700

    Config didn't work

--- hosts/abaddon/kubernetes.nix
+++ hosts/abaddon/kubernetes.nix
@@ -9,7 +9,7 @@
   services.k3s = {
     enable = true;
     role = "agent";
-    serverAddr = lib.mkDefault "https://${config.networking.sheol.currenthost.ipv4}:6443";
+    serverAddr = lib.mkDefault "https://10.0.2.10:6443";
     token = lib.mkDefault config.age.secrets."passwords/k3s/token".path;
     extraFlags = toString [
       "--node-ip ${config.networking.abaddon.currenthost.ipv4}"
