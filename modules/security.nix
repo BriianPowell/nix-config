b281885cbfb74c416fb0642c8bfcef559841b90c
Brian Powell <brian@powell.place>
2025-05-08 12:58:32 -0700

    Sudo?

--- modules/security.nix
+++ modules/security.nix
@@ -1,10 +1,18 @@
-{ ... }: {
+{ pkgs, ... }: {
   security = {
     sudo.enable = false;
-    sudo-rs = {
-      enable = true;
-      execWheelOnly = true;
-      wheelNeedsPassword = false;
+    # sudo-rs = {
+    #   enable = true;
+    #   execWheelOnly = true;
+    #   wheelNeedsPassword = false;
+    # };
+    wrappers.sudo-rs = {
+      #source = "${lib.getExe pkgs.sudo-rs}";
+      source = "${pkgs.sudo-rs}/bin/sudo";
+      setuid = true;
+      setgid = true;
+      owner = "0";
+      group = "0";
     };
   };
 }
