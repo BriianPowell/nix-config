d5babcd517f2ddbae26e44bc9d64b5a057a6bf0d
Brian Powell <brian@powell.place>
2025-05-08 13:04:51 -0700

    No sudo wrpaper

--- modules/security.nix
+++ modules/security.nix
@@ -6,13 +6,13 @@
     #   execWheelOnly = true;
     #   wheelNeedsPassword = false;
     # };
-    wrappers.sudo = {
-      #source = "${lib.getExe pkgs.sudo-rs}";
-      source = "${pkgs.sudo-rs}/bin/sudo";
-      setuid = true;
-      setgid = true;
-      owner = "0";
-      group = "0";
-    };
+    # wrappers.sudo = {
+    #   #source = "${lib.getExe pkgs.sudo-rs}";
+    #   source = "${pkgs.sudo-rs}/bin/sudo";
+    #   setuid = true;
+    #   setgid = true;
+    #   owner = "0";
+    #   group = "0";
+    # };
   };
 }
