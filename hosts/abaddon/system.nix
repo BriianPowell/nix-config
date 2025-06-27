92870f591fd9501e438260de0fd5674cb4fc5f31
Brian Powell <brian@powell.place>
2025-06-26 17:45:48 -0700

    Abaddon host file

--- hosts/abaddon/system.nix
+++ hosts/abaddon/system.nix
@@ -2,4 +2,9 @@
   networking.hostName = "abaddon";
   networking.useDHCP = false;
   networking.interfaces.enp0s31f6.useDHCP = true;
+
+  networking.hosts = {
+    "127.0.0.2" = "abaddon";
+    "10.0.2.11" = "abaddon";
+  };
 }
