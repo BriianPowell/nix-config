36e1f05b32cedeb063424bf0eb4c57195782a0a4
Brian Powell <brian@powell.place>
2025-06-26 17:50:34 -0700

    List of string

--- hosts/abaddon/system.nix
+++ hosts/abaddon/system.nix
@@ -4,7 +4,7 @@
   networking.interfaces.enp0s31f6.useDHCP = true;
 
   networking.hosts = {
-    "127.0.0.2" = "abaddon";
-    "10.0.2.11" = "abaddon";
+    "127.0.0.2" = [ "abaddon" ];
+    "10.0.2.11" = [ "abaddon" ];
   };
 }
